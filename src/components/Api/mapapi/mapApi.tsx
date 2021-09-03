ca4892645284ccbd68e210ce9f57045f61e977df
SimonDevelope <whwnine@gmail.com>
2021-09-03 18:00:00 +0900

    #7. add up zoom control, map type control

--- src/components/Api/mapapi/mapApi.tsx
+++ src/components/Api/mapapi/mapApi.tsx
@@ -12,11 +12,29 @@ const mapApi = (): ReactElement => {
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      center: new window.kakao.maps.LatLng(37.56837079623827, 126.98866373015571),
       level: 3,
     };
-    new window.kakao.maps.Map(container, options);
+    const map = new window.kakao.maps.Map(container, options);
+
+    //지도 컨트롤러
+    const mapTypeControl = new window.kakao.maps.MapTypeControl();
+    map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
+    const zoomControl = new window.kakao.maps.ZoomControl();
+    map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+
+    const marker = new window.kakao.maps.Marker({
+      position: map.getCenter(),
+    });
+    marker.setMap(map);
+
+    //지도 클릭 이벤트
+    window.kakao.maps.event.addListener(map, 'click', function (mouseEvent: any) {
+      const latlng = mouseEvent.latLng;
+      marker.setPosition(latlng);
+    });
   });
+
   return <div id="map" className="map-api-style"></div>;
 };
 
