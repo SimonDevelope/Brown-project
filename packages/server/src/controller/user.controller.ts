cb585e95451b89b646a2966cfd46aca1d6266d11
SimonDevelope <whwnine@gmail.com>
2021-09-18 18:42:40 +0900

    #3.

---
+++ packages/server/src/controller/user.controller.ts
@@ -0,0 +1,15 @@
+import { getRepository } from "typeorm";
+import { Photo } from "../entity/User";
+
+export const createUser = async () => {
+  const userRepo = getRepository(Photo);
+  const user = userRepo.create({
+    firstName: "Lee",
+    lastName: "authman",
+    specify: 27,
+  });
+  await userRepo.save(user).catch((error) => {
+    console.log("ERROR :", error);
+  });
+  console.log("New User Saved With Id : ", user);
+};
