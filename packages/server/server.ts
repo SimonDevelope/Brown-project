a04c1bb846bc480547c72d7fb37f6e4109c98262
SimonDevelope <whwnine@gmail.com>
2021-09-18 18:45:44 +0900

    #@

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -5,26 +5,27 @@ const cors = require("cors");
 const port = process.env.DB_PORT;
 const app = express();
 
-import { Request, Response } from "express";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import ConnectionOptions from "./ormconfig";
+import { createUser } from "./src/controller/user.controller";
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+app.post("/createPhoto", createUser);
+
 createConnection(ConnectionOptions)
-  .then(() => {
+  .then(async () => {
     console.log("connected!");
+    await createUser().catch((error) => {
+      console.log(error);
+    });
   })
   .catch((error) => {
     console.log(error);
   });
-
-app.get("/", async (req: Request, res: Response) => {
-  res.send("hi");
-});
 app.listen(port, () => {
   console.log("the server running");
 });
